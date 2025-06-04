fc4c4db6cfb3ed10242b64ad31c5c39352e0a90e
axel <sagit117@gmail.com>
2025-06-04 08:53:05 +0300

    add json encoder

--- src/api/api.go
+++ src/api/api.go
@@ -1,14 +1,28 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
 	"ru.axel.pro.project/config"
 )
 
+type MenuSide struct {
+	Project string
+}
+
 func Api_v1(server *http.ServeMux, app *config.Application) {
 	server.HandleFunc("GET /api/v1/get/menu/side", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "{\"test\":\"test\"}")
+		menu := &MenuSide{
+			Project: "Проекты",
+		}
+
+		data, err := json.Marshal(menu)
+		if err != nil {
+			app.Logger.Fatal(err)
+		}
+
+		fmt.Fprint(w, string(data))
 	})
 }
